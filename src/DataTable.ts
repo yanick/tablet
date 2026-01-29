8fc96b6460517b3c66bf49a95f894e6ad534a58c
Yanick Champoux <yanick@babyl.ca>
2026-01-29 12:07:10 -0500

    wip

--- src/DataTable.ts
+++ src/DataTable.ts
@@ -1,4 +1,5 @@
-import fs from 'fs';
+import fs from 'fs/promises';
+import { extname } from 'path';
 import frontmatter from 'frontmatter';
 import { createMarkdownObjectTableSync } from 'parse-markdown-table';
 import u from '@yanick/updeep';
@@ -15,51 +16,104 @@ type Metadata = {
 	subtable?: string
 };
 
+
+
+type ParseFile = <ENTRY = any>(content: string) => Promise<{ entries: ENTRY[], metadata?: Metadata }>
+type SerializeFile = <ENTRY = any>(table: DataTable<ENTRY>) => Promise<string>
+
+async function readMarkdown(content: string) {
+	const parsed = frontmatter(content);
+
+	const metadata = parsed.data;
+	const entries = [...createMarkdownObjectTableSync(parsed.content)];
+
+	return { metadata, entries };
+}
+
+async function readYAML(content: string) {
+	const parsed: any = yaml.parseAllDocuments(
+		content
+	).map(x => x.toJSON());
+
+	if (parsed.length < 2) parsed.unshift({});
+
+	const [metadata, entries] = parsed;
+
+	return { metadata, entries };
+}
+
+
+function groomEntries(entries) {
+	for (const e of entries) {
+		if (e.roll?.includes('-')) {
+			e.roll = e.roll.split('-').map(x => Number(x))
+		}
+		if (e.roll?.includes(',')) {
+			e.roll = e.roll.split(',').map(x => Number(x))
+		}
+	}
+
+	return entries;
+}
+
+
 export default class DataTable<ENTRY = Record<string, any>, META extends Metadata = Metadata> {
 	path: string;
-	metadata: META;
-	entries: ENTRY[];
+	metadata: Promise<META>;
+	entries: Promise<ENTRY[]>;
+	fileType: 'md';
+
+	fileApis = [
+		{ ext: '.md', read: readMarkdown, },//serialize: writeMarkdown },
+		{ ext: ['.yaml', '.yml'], read: readYAML },//, serialize: writeYAML },
+	];
 
 	constructor(path: string) {
 		this.path = path;
 
-		const parsed = frontmatter(
-			fs.readFileSync(path, { encoding: 'utf8' })
-		);
+		const ext = extname(path);
 
-		this.metadata = parsed.data;
-		this.entries = [...createMarkdownObjectTableSync(parsed.content)] as any as ENTRY[];
-
-		for (const e of (this.entries as any[])) {
-			if (e.roll?.includes('-')) {
-				e.roll = e.roll.split('-').map(x => Number(x))
-			}
-			if (e.roll?.includes(',')) {
-				e.roll = e.roll.split(',').map(x => Number(x))
-			}
+		const fileApi = findApi(ext);
+		if (!fileApi) {
+			throw new Error(`extension '${ext}' not recognized`);
 		}
 
+		const file = fs.readFile(path, { encoding: 'utf8' }).then(
+			fileApi.read
+		);
+		this.entries = file.then(R.prop('entries')).then(
+			groomEntries
+		) as Promise<ENTRY[]>;
+		this.metadata = file.then(R.prop('metadata'));
+	}
+
+	findApi(ext: string) {
+		return this.fileApis.find(
+			api => Array.isArray(api.ext) ? api.ext.includes(ext) : ext === api.ext
+		);
 	}
 
 	async roll(value?: number): Promise<ENTRY> {
+		const metadata: any = await this.metadata;
 		if (!value) {
-			if (!(this.metadata as any)?.roll)
+			if (!metadata?.roll)
 				throw new Error('roll metadata not defined for table');
 
-			value = roll.roll((this.metadata as any).roll).result;
+			value = roll.roll(metadata.roll).result;
 		}
 
 		// @ts-ignore
-		const entry = this.entries.find(({ roll }: { roll: number | [number, number] }) => {
+		const entry = (await this.entries).find(({ roll }: { roll: number | [number, number] }) => {
 			if (Array.isArray(roll))
 				return roll[0] <= value && roll[1] >= value;
 			return roll == value;
 		}) ?? await this.populateTableFor(value);
 
-		if (this.metadata.subtable) {
-			const subtable = entry[this.metadata.subtable];
-			if (subtable)
-				return new DataTable<ENTRY>(subtable).roll();
+		const { subtable } = await this.metadata;
+		if (subtable) {
+			const file = entry[subtable];
+			if (file)
+				return new DataTable<ENTRY>(file).roll();
 		}
 
 		return entry;
@@ -71,7 +125,7 @@ export default class DataTable<ENTRY = Record<string, any>, META extends Metadat
 
 		let entry = {} as ENTRY;
 
-		const [sample] = this.entries;
+		const [sample] = await this.entries;
 
 		for (const key in sample) {
 			const result = await prompt.input({ message: key });
@@ -79,8 +133,7 @@ export default class DataTable<ENTRY = Record<string, any>, META extends Metadat
 			entry[key] = result;
 		}
 
-		this.entries.push(entry);
-		this.save();
+		await this.entries.then(e => e.push(entry)).then(() => this.save());
 
 		return entry;
 	}
@@ -103,6 +156,8 @@ export default class DataTable<ENTRY = Record<string, any>, META extends Metadat
 
 		const content = `---\n${meta}\n---\n${table}\n`;
 
-		return fs.writeFileSync(this.path, content);
+		return fs.writeFile(this.path, content);
 	}
+
+
 }
